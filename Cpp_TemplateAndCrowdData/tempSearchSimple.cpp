3e47046b24fbea11ae6b70a4cbd74b4e3cf5f996
lanseria <zhangchao564265135@hotmail.com>
2017-12-26 12:23:27 +0800

    finish 9 chapter

---
+++ Cpp_TemplateAndCrowdData/tempSearchSimple.cpp
@@ -0,0 +1,8 @@
+template <class T>
+int seqSearch(const T list[], int n, const T &key)
+{
+  for (int i = 0; i < n; i++)
+    if (list[i] == key)
+      return i;
+  return -1;
+}
---
+++ Cpp_TemplateAndCrowdData/tempSwap.cpp
@@ -0,0 +1,23 @@
+template <class T>
+void mySwap(T &x, T &y)
+{
+  T temp = x;
+  x = y;
+  y = temp;
+}
+template <class T>
+void bubbleSort(T a[], int n)
+{
+  int i = n – 1;
+  while (i > 0)
+  {
+    int lastExchangeIndex = 0;
+    for (int j = 0; j < i; j++)
+      if (a[j + 1] < a[j])
+      {
+        mySwap(a[j], a[j + 1]);
+        lastExchangeIndex = j;
+      }
+    i = lastExchangeIndex;
+  }
+}
